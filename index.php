8206ca2c912f708d8df05da9d8db188d93017941
Rafes <rafadvcoliveira@gmail.com>
2022-05-26 23:28:26 -0300

    Resolve exercise4

--- index.php
+++ index.php
@@ -1,29 +1,8 @@
 <?php
-// não se esqueça, para iniciar o programa use php e o nome desse arquivo (index.php)
-$name = readline('Digite seu nome: ');
-$age = (int)readline('Digite sua idade: ');
-
-echo ("Nome: {$name}, Idade: {$age}; \n");
-
-
-if($age > 18) {
-    echo "Verdadeiro \n";
-};
-
-if($age !== 25) {
-    echo "Falso \n";
-};
-
-if($name === "Marcos" && $age !== 25) {
-    echo "Falso \n";
-};
-
-if($age !== 25 || $name === "Marcos") {
-    echo "Verdadeiro \n";
-};
-
-if($age % 2 === 0) {
-    echo "Verdadeiro \n";
-};
+    $balance = 200.00;
+    $deposit = (float)readline("Quanto deseja depositar? ");
+    echo("Saldo anterior: {$balance} \n");
+    $balance += $deposit;
+    echo("Seu saldo atual: {$balance}");
 ?>
 
