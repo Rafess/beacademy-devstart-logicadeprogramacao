d267a4a2373c0a98fb6696c0bea08412e73504a2
Rafes <rafadvcoliveira@gmail.com>
2022-05-26 22:54:34 -0300

    Resolve exercise2

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 // não se esqueça, para iniciar o programa use php e o nome desse arquivo (index.php)
-$address = readline('Digite seu endereço: ');
-
-echo $address;
+$name = readline('Digite seu nome: ');
+$hight = (float)readline('Digite sua altura em metros: ');
+$weight = (float)readline('Digite seu peso em kg: ');
+$IMC = ($weight / ($hight ** 2)); // imc = peso dividido pela altura ao quadrado;
+echo ("Nome: {$name}, Altura: {$hight}m, Peso: {$weight}kg, IMC: {$IMC}.");
 
 
 ?>
