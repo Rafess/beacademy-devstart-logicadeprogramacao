d2a0058a91cc720e5e0115bd98265ea8b0fa994d
Rafes <rafadvcoliveira@gmail.com>
2022-05-27 00:22:08 -0300

    Resolve Exercise8

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-$number1 = readline("Digite o primeiro número: ");
-$number2 = readline("Digite o segundo número: ");
-$operation = readline("Digite a operação que deseja fazer( +, -, *, /): ");
+$figure = readline("Escolha a figura geométrica desejada: Q-Quadrado T-Triangulo: ");
 
-switch ($operation) {
-    case "+" :
-        $result = $number1 + $number2;
-        echo("{$number1} {$operation} {$number2} = {$result}");
-    case "-" : 
-        $result = $number1 - $number2;
-        echo("{$number1} {$operation} {$number2} = {$result}");
-    case "/" : 
-        $result = $number1 / $number2;
-        echo("{$number1} {$operation} {$number2} = {$result}");
-    case "*" : 
-        $result = $number1 * $number2;
-        echo("{$number1} X {$number2} = {$result}");
-
-}
+switch ($figure) {
+    case "T" :
+        $base = readline("Digite a medida da base: ");
+        $sideA = readline("Digite a medida do lado do triangulo: ");
+        $sideB = readline("Digite a medida do outro lado do triangulo: ");
+        $height = readline("Digite a altura: ");
+        $area = ($base * $height);
+        $perimeter = ($base + $sideA + $sideB);
+        echo("A área do triangulo é: {$area} \n E o perímetro é: {$perimeter}");
+        break;
+    case "Q" : 
+        $side = readline("Digite a medida do lado do quadrado: ");
+        $area = ($side ** 2);
+        $perimeter = ($side * 4);
+        echo("A área do quadrado é: {$area} \n E o perímetro é: {$perimeter}");
+        break;
+   }
 ?>
 
