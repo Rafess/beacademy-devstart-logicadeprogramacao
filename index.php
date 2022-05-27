888dbb7500dd8a5f10eba778839dc08ed01aef45
Rafes <rafadvcoliveira@gmail.com>
2022-05-27 00:40:00 -0300

    Resolve exercise10

--- index.php
+++ index.php
@@ -1,33 +1,10 @@
 <?php
-$balance = 200.00;
-$operation = (int)readline("Qual operação deseja fazer: 1- Desposito; 2- Saque, 3- Transferência;\n");
-$value = (int)readline("Qual o valor da operação? ");
-switch ($operation) {
-    case 1:
-        echo("Seu saldo inicial: {$balance} \n");
-        echo("Operação realizada: Deposito \n");
-        $balance += $value;
-        echo("Saldo atual: {$balance}");
-        break;
-    case 2: 
-        echo("Seu saldo inicial: {$balance} \n");
-        echo("Operação realizada: Saque \n");
-        $balance -= $value;
-        echo("Saldo atual: {$balance}");
-        break;
-    case 3:
-        $bank = readline("Qual o nome do banco? ");
-        $agency = readline("Qual o número da agência? ");
-        $account = readline("Qual o número da conta? ");
-        echo("Seu saldo inicial: {$balance} \n");
-        echo("Operação realizada: Transferência para: \n
-        Banco: {$bank} \n
-        Agência: {$agency} \n
-        Conta: {$account} \n
-        Valor: {$value} \n");
-        $balance -= $value;
-        echo("Saldo atual: {$balance}");
-        break;
+$value = (int)readline("Digite o número que deseja calcular a tabuada: ");
+
+for($i = 1; $i <= 10; $i += 1) {
+    $result = $i * $value;
+    echo("{$value} x {$i} = {$result} \n");
 }
+
 ?>
 
