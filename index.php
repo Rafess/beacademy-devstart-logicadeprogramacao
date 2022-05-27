20066960c1665f6ca9e758a1252ea7e26fb99ee9
Rafes <rafadvcoliveira@gmail.com>
2022-05-26 23:41:05 -0300

    Resolve exercise6

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
-// não se esqueça, para iniciar o programa use php e o nome desse arquivo (index.php)
-$hight = (float)readline('Digite sua altura em metros: ');
-$weight = (float)readline('Digite seu peso em kg: ');
-$IMC = ($weight / ($hight ** 2)); // imc = peso dividido pela altura ao quadrado;
 
-if($IMC < 19) {
-    echo("Você está abaixo do peso! Seu IMC é de {$IMC}");
-} else if($IMC >= 19 && $IMC <25) {
-    echo("Você está com o peso normal! Seu IMC é de {$IMC}");
-} else if($IMC >= 25 && $IMC <30) {
-    echo("Você está com sobrepeso! Seu IMC é de {$IMC}");
-} else if($IMC >= 30 && $IMC < 40) {
-    echo("Você está com obesidade tipo I! Seu IMC é de {$IMC}");
+$age = (int)readline('Digite sua idade: ');
+
+if($age < 19) {
+    echo("Menor de idade!");
+} else if($age >= 18 && $age < 60) {
+    echo("Adulto");
 } else {
-    echo("Você está com obesidade mórbida! Seu IMC é de {$IMC}");
+    echo("Idoso");
 }
 
 
