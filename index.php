ca55181e3591048bc1e898691d970c38af7f83a1
Rafes <rafadvcoliveira@gmail.com>
2022-05-27 00:54:02 -0300

    Resolve exercise 13

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
-$initialValue = (int)readline("Digite o valor incial: ");
-$finalValue = (int) readline("Digite o valor final: ");
+$balance = 200.00;
+$times = (int)readline("Quantas vezes deseja repetir? ");
 
-for($i = $initialValue; $i <= $finalValue; $i += 1) {
-    if($i % 3 === 0) {
-        echo("{$i} \n");
+for($i = 0; $i < $times; $i+=1) {
+    $operation = (int)readline("Qual operação deseja fazer: 1- Desposito; 2- Saque, 3- Transferência; \n");
+    $value = (int)readline("Qual o valor da operação? ");
+    switch ($operation) {
+        case 1:
+            echo("Seu saldo inicial: {$balance} \n");
+            echo("Operação realizada: Deposito \n");
+            $balance += $value;
+            echo("Saldo atual: {$balance}");
+            break;
+        case 2: 
+            echo("Seu saldo inicial: {$balance} \n");
+            echo("Operação realizada: Saque \n");
+            $balance -= $value;
+            echo("Saldo atual: {$balance}");
+            break;
+        case 3:
+            $bank = readline("Qual o nome do banco? ");
+            $agency = readline("Qual o número da agência? ");
+            $account = readline("Qual o número da conta? ");
+            echo("Seu saldo inicial: {$balance} \n");
+            echo("Operação realizada: Transferência para: \n
+            Banco: {$bank} \n
+            Agência: {$agency} \n
+            Conta: {$account} \n
+            Valor: {$value} \n");
+            $balance -= $value;
+            echo("Saldo atual: {$balance}");
+            break;
     }
 }
-
 ?>
 
