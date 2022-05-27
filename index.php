cd9ef45c827a497bda2b78bc5ee20b165394bc1e
Rafes <rafadvcoliveira@gmail.com>
2022-05-27 00:46:07 -0300

    Resolve exercise12

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+$initialValue = (int)readline("Digite o valor incial: ");
+$finalValue = (int) readline("Digite o valor final: ");
 
-for($i = 0; $i <= 100; $i += 1) {
-    if($i % 2 === 0) {
+for($i = $initialValue; $i <= $finalValue; $i += 1) {
+    if($i % 3 === 0) {
         echo("{$i} \n");
     }
 }
